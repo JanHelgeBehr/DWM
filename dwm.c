651568af4104233e27884d315cddd4c98e7788e7
JanHelgeBehr <87608744+JanHelgeBehr@users.noreply.github.com>
2022-10-13 18:48:42 +0000

    Update dwm.c

--- dwm.c
+++ dwm.c
@@ -62,7 +62,7 @@
 
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
-enum { SchemeNorm, SchemeSel, SchemeWarn, SchemeUrgent }; /* color schemes */
+enum { SchemeNorm, SchemeSel, SchemeRed, SchemeYellow, SchemeGreen }; /* color schemes */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
        NetWMWindowTypeDialog, NetClientList, NetLast }; /* EWMH atoms */
